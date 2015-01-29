dceb1c55362710e0111e9ac973bd188dce17e17f
Pa-Essa Jabang <pjabang@gmail.com>
2015-01-29 18:35:32 +0000

    added colors

--- src/js/module/mrt/geomodel/controllers/geomodel-ctrl.js
+++ src/js/module/mrt/geomodel/controllers/geomodel-ctrl.js
@@ -1,74 +1,113 @@
 angular.module('MRT').controller('GeoModelCtrl', ['$scope', '$filter', 'geoModelService', 'geoGroupService', function($scope, $filter, geoModelService, geoGroupService) {
-        $scope.data = {weight: 10};
-        $scope.response = {};
-        $scope.response.geogroups = {data: []};
-        $scope.response.geoindicators = {list: []};
-        $scope.data.values = {};
-        $scope.data.scoreMax = 100;
-        $scope.data.indicators = [];
-        $scope.data.params = {id: 1, date: 2013};
-        $scope.predicate = '-score';
+		$scope.data = {weight: 10};
+		$scope.response = {};
+		$scope.response.geogroups = {data: []};
+		$scope.response.geoindicators = {list: []};
+		$scope.data.values = {};
+		$scope.data.maxGeoScore = 0;
+		$scope.data.scoreMax = 100;
+		$scope.data.indicators = [];
+		$scope.data.params = {id: 1, date: 2013};
+		$scope.predicate = '-score';
+
+		$scope.data.dates = [2013, 2012, 2011, 2010];
+
+		$scope.data.params.date = $scope.data.dates[0];
+		// Here we are referencing the same object, so Angular inits the select box correctly
+
+		$scope.getIndicatorValues = function(model, item) {
+			$scope.data.params.geogroup_id = item.id;
+			$scope.getValues();
+		};
+
+		$scope.getValues = function() {
+			geoModelService.getValues($scope.data.params).success(function(data) {
+
+				$scope.data.values = data;
+				$scope.setIndicators();
+
+			}).error(function(error) {
+				$scope.status = 'Unable to load customer data: ' + error.message;
+
+			});
+		};
+
+		// get indicators
+		geoModelService.getIndicators($scope.data.params).success(function(data) {
+
+			$scope.response.geoindicators.list = data.indicators;
+			$scope.setIndicators();
 
 
-        $scope.getValues = function(model, item) {
-            $scope.data.params.geogroup_id = item.id;
-            geoModelService.getValues($scope.data.params).success(function(data) {
+		}).error(function(error) {
+			$scope.status = 'Unable to load customer data: ' + error.message;
 
-                $scope.data.values = data;
-                $scope.setIndicators();
+		});
 
-            }).error(function(error) {
-                $scope.status = 'Unable to load customer data: ' + error.message;
+		geoGroupService.list().success(function(data) {
+			$scope.response.geogroups.data = data.geogroups;
 
-            });
-        };
 
-        // get indicators
-        geoModelService.getIndicators($scope.data.params).success(function(data) {
+		}).error(function(error) {
+			$scope.status = 'Unable to load groups data: ' + error.message;
 
-            $scope.response.geoindicators.list = data.indicators;
+		});
 
-        }).error(function(error) {
-            $scope.status = 'Unable to load customer data: ' + error.message;
+		$scope.setIndicators = function() {
+			$scope.indicatorChanged();
+			// $scope.data.indicators = $scope.data.values.indicators;
+			angular.forEach($scope.response.geoindicators.list, function(indicator, key) {
+				indicator.max = 0;
+				indicator.maxScore = 0;
+			});
+		};
+		$scope.indicatorChanged = function(indicator) {
+			$scope.data.maxGeoScore = 0;
+			if (typeof indicator !== "undefined") {
+				indicator.max = 0;
+				indicator.maxScore = 0;
+			}
+			// alert("hey");
+		};
 
-        });
 
-        geoGroupService.list().success(function(data) {
-            $scope.response.geogroups.data = data.geogroups;
 
-        }).error(function(error) {
-            $scope.status = 'Unable to load groups data: ' + error.message;
+		$scope.getScore = function(geography) {
+			var $return = 0;
+			var per = $scope.response.geoindicators.list.length * this.data.values.settings.max_weight;
+			angular.forEach($scope.response.geoindicators.list, function(indicator, key) {
 
-        });
-        $scope.setIndicators = function() {
-            // $scope.data.indicators = $scope.data.values.indicators;
-        };
+				var found = $filter('filter')(geography.indicators, {geography_id: geography.id, geoindicator_id: indicator.id}, true);
+				// console.log(indicator);
+				if (found.length) {
+					var weight = ((indicator.weight / per) * 100);
+					// $return = indicator.weight;
+					found[0].score = (($scope.data.scoreMax / found[0].value) * weight);
 
-        $scope.getScore = function(geography) {
-            var $return = 0;
-            var per = $scope.response.geoindicators.list.length * this.data.values.settings.max_weight;
-            angular.forEach($scope.response.geoindicators.list, function(indicator, key) {
+					if (found[0].score > indicator.maxScore) {
+						indicator.max = found[0].value;
+						indicator.maxScore = found[0].score;
+					}
+					$return += found[0].score;
+				}
+			});
+			geography.score = $return;
+			if (geography.score > $scope.data.maxGeoScore) {
+				$scope.data.maxGeoScore = geography.score;
+			}
 
-                var found = $filter('filter')(geography.indicators, {geography_id: geography.id, geoindicator_id: indicator.id}, true);
-                // console.log(indicator);
-                if (found.length) {
-                    var weight = ((indicator.weight / per) * 100);
-                    // $return = indicator.weight;
-                    $return += (($scope.data.scoreMax / found[0].value) * weight);
-                }
-            });
-            geography.score = $return;
-            return $return;
-        }
+			return $return;
+		}
 
-        $scope.getGeographyIndicator = function(geography, indicator) {
-            var found = $filter('filter')(geography.indicators, {geography_id: geography.id, geoindicator_id: indicator.id}, true);
-            // console.log(found);
-            if (found.length) {
-                return found[0].value;
-            } else {
-                return '';
-            }
-        }
+		$scope.getGeographyIndicator = function(geography, indicator) {
+			var found = $filter('filter')(geography.indicators, {geography_id: geography.id, geoindicator_id: indicator.id}, true);
+			// console.log(found);
 
-    }]);
+			if (found.length) {
+				return found[0];
+			} else {
+				return {};
+			}
+		}
+
+	}]);
--- src/js/module/mrt/parameters.js
+++ src/js/module/mrt/parameters.js
@@ -1,2 +1,2 @@
 angular.module('app-parameters',[])  
-  .constant('env', {"apiUrl":"http://10.51.130.212/mrt/web/app_dev.php/api/"});
+  .constant('env', {"apiUrl":"http://192.168.0.7/market-attractiveness/mrt-api/web/app.php/api"});
